98d36d4941c9b16a7c444931d2f470c7e493782f
Clark Ngo <clarkngo@gmail.com>
2019-10-24 19:53:11 -0700

    magic rounding

---
+++ round/README.md
@@ -0,0 +1,7 @@
+# Rounding Up and Down without any functions
+```
+// round up
+cout << int(2.4 + 0.5) << endl;
+// round down
+cout << int(2.6 + 0.5) << endl;
+```
---
+++ round/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // round up
+  cout << int(2.4 + 0.5) << endl;
+  // round down
+  cout << int(2.6 + 0.5) << endl;
+  return 0;
+}
