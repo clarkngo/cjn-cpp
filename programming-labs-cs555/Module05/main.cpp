a570043f3fd175454c59950693d74aafd59f2524
Clark Ngo <clarkngo@gmail.com>
2019-11-11 15:11:45 -0800

    test gitignore

--- programming-labs-cs555/Module05/main.cpp
+++ programming-labs-cs555/Module05/main.cpp
@@ -1,12 +1,9 @@
 // Author @Clark Jason Ngo
 #include <iostream>
-#include "guess.h"
-using namespace std;		// program uses from the std namespace
-							// function main begins the program
+using namespace std;
+
 int main()
 {
-    GuessingGame g1;
-
-    g1.run();
-    return 0;	    	// exit code of the program
+    cout << "Hello";
+    return 0;
 }
